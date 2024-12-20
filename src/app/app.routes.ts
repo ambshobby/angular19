6aca98bd2fff40a4b6df79a470e4a7bd0d9df6c3
ambshobby <ambs.hobby@gmail.com>
2024-12-20 06:48:57 +0530

    first a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,23 @@
 import { Routes } from '@angular/router';
 import { ArrayvalueComponent } from './componentspractice/arrayvalue/arrayvalue.component';
 import { DemoComponent } from './demo/demo.component';
+import { SinglevalueComponent } from './componentspractice/singlevalue/singlevalue.component';
 
 export const routes: Routes = [
     { 
         path: '',
         title: 'App Home Page',
-        component:DemoComponent,
+        component:DemoComponent
         
     },
     {
         path: 'arrayval',
         title: 'App Home Page',
-        component:ArrayvalueComponent,
+        component:ArrayvalueComponent
+    },
+    {
+        path:'singleval',
+        title:'single value componenet invoked',
+        component:SinglevalueComponent
     }
 ];
